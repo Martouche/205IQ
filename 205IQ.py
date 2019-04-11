ad226473c729cf516bf7191c52301f7d9930c5bf
vantalon <martin.vantalon@epitech.eu>
2019-04-11 19:36:33 +0200

    first

---
+++ 205IQ.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import sys, math, time
+from sys import stdin
+from math import factorial, sqrt, exp, floor, pi
+
+def print_help():
+    print("USAGE")
+    print("\t./205IQ u s [IQ1] [IQ2]\n")
+    print("DESCRIPTION")
+    print("\tu mean")
+    print("\ts standard deviation")
+    print("\tIQ1 minimum IQ")
+    print("\tIQ2 maximum IQ")
+
+def first_calcul(x, tab1):
+    res = exp(-pow((x - tab1[0]) / tab1[1], 2) / 2) / (tab1[1] * sqrt(2 * pi))
+#    print(x, format(res * 100, ".2f"))
+    return res
+
+def error_handling():
+    tab = []
+    i = 1
+    try:
+        while (i < len(sys.argv)):
+            tab.append(int(sys.argv[i]))
+            i += 1
+    except:
+        print_help()
+        exit(84)
+    return tab
+
+def loopIQ(tab):
+    x = 0
+    if len(tab) == 2:
+        print(tab)
+        while (x <= 200):
+            first_calcul(x, tab)
+            x += 1
+    elif len(tab) == 3:
+        print("FUCK", tab)
+    elif len(tab) == 4:
+        print("FUCK2", tab)
+    i = 0
+
+def main():
+    if len(sys.argv) >= 3 and len(sys.argv) <= 5:
+        arg = error_handling()
+    else:
+        return 84
+    loopIQ(arg)
+    return 0
+
+main()
